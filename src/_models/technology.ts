43b6e45a60052d6e115d493c373ac02a71d6bba0
Michał Rykowski <rykos9999@gmail.com>
2020-09-13 19:22:25 +0200

    init

---
+++ src/_models/technology.ts
@@ -0,0 +1,12 @@
+export interface Technology{
+    name: string;
+    description: string;
+    iconUrl: string;
+    type?: TechnologyType;
+}
+
+export enum TechnologyType{
+    backend,
+    frontend,
+    other
+}
